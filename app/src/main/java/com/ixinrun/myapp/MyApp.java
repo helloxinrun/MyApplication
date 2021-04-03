4ae74f7259e6c66dde5fa77d576da906937fb934
toperc <642545912@qq.com>
2021-04-04 03:50:45 +0800

    refect: 添加相关工具类

---
+++ app/src/main/java/com/ixinrun/myapp/MyApp.java
@@ -0,0 +1,11 @@
+package com.ixinrun.myapp;
+
+import com.ixinrun.app_base.BaseApplication;
+
+public class MyApp extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
